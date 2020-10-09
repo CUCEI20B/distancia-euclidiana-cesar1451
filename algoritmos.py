5756139427692842748f796eafb15ee198d92e58
César González <cesar.gonzalez7978@alumnos.udg.mx>
2020-10-09 16:54:32 -0500

    Modificada la funcion quitandole el pow por **

--- algoritmos.py
+++ algoritmos.py
@@ -1,19 +1,6 @@
 import math
 
 def distancia_euclidiana(x_1, y_1, x_2, y_2):
-    return math.sqrt(pow((x_2 - x_1), 2) + pow((y_2 - y_1), 2))
-    """ Calcula la distancia euclidiana
-
-    Devuelve el resultado de la formula
-
-    También se le conoe a la fórmula como:
-    distancia entre dos puntos
-
-    Parámetros:
-    x_1 -- origen_x
-    y_1 -- origen_y
-    x_2 -- destino_x
-    y_2 -- destino_y
-
-    """
+    return math.sqrt((x_2 - x_1)**2 + (y_2 - y_1)**2)
+    
     
